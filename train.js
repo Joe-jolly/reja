17a26ccaac8ce1b4d249c6806f6e9e2d0db0f589
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2024-11-21 20:56:50 +0900

    train problem solving - A TASK

--- train.js
+++ train.js
@@ -1,40 +1,91 @@
+/* A Task */
+
+/* Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi. 
+MASALAN countLetter("e", "engineer") 3ni return qiladi. */
+
+// Masala yechimi -1
+
+// Declaretion function
+function letterCount(letter, word) {
+    const lettersArray = word.split('');
+    let count = 0;
+
+    for (let inputLetter of lettersArray) {
+        if (inputLetter === letter) {
+            count++;
+        }
+    }
+    return count;
+}; 
+
+console.log("kiritilingan harfdan jami:", letterCount("e", "engineer"), "ta bor");
+
+// // Expression function
+// const letterCount = function(letter, word) {
+//     const lettersArray = word.split('');
+//     let count = 0;
+
+//     for (let inputLetter of lettersArray) {
+//         if (inputLetter === letter) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }; 
+
+// console.log("kiritilingan harfdan jami:", letterCount("e", "engineer"), "ta bor");
+
+// // Arrow function
+// const letterCount = (letter, word) => {
+//     const lettersArray = word.split('');
+//     let count = 0;
+
+//     for (let inputLetter of lettersArray) {
+//         if (inputLetter === letter) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }; 
+
+// console.log("kiritilingan harfdan jami:", letterCount("e", "engineer"), "ta bor");
 
 /* 22. Asynchronous functionlarni qo'llash */
 
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba bo'ling", // 0-20
-    "to'g'ri boahliq tanlag va ko'proq xato qiling", // 20-30
-    "o'zingiz uchun ishlashni boshlang", // 30-40
-    "siz kuchli bo'lgan narsalarni qiling", // 40-50
-    "yoshlarga invistitsiya qiling", // 50-60
-    "endi dam oling, endi foydasi yo'q", //60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba bo'ling", // 0-20
+//     "to'g'ri boahliq tanlag va ko'proq xato qiling", // 20-30
+//     "o'zingiz uchun ishlashni boshlang", // 30-40
+//     "siz kuchli bo'lgan narsalarni qiling", // 40-50
+//     "yoshlarga invistitsiya qiling", // 50-60
+//     "endi dam oling, endi foydasi yo'q", //60
+// ];
 
-// ASYNC function
-async function maslahatBering(a) {
-    if (typeof a !== 'number') throw new Error("insert a number");
-    else if (a <= 20) return list[0];
-    else if (a > 20 && a <= 30) return list[1];
-    else if (a > 30 && a <= 40) return list[2];
-    else if (a > 40 && a <= 50) return list[3];
-    else if (a > 50 && a <= 60) return list[4];
-    else {
-        //return list [5];
+// // ASYNC function
+// async function maslahatBering(a) {
+//     if (typeof a !== 'number') throw new Error("insert a number");
+//     else if (a <= 20) return list[0];
+//     else if (a > 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//     else {
+//         //return list [5];
 
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
 
-            }, 5000)
-           resolve (list [5]); 
-        }) 
+//             }, 5000)
+//            resolve (list [5]); 
+//         }) 
                 
-        // setTimeout(function () {
-        //     callback( null, list [5] );
-        // }, 5000);
-    }
-}
+//         // setTimeout(function () {
+//         //     callback( null, list [5] );
+//         // }, 5000);
+//     }
+// }
 
 // // call via then/catch
 // console.log("passed here 0");
@@ -48,16 +99,16 @@ async function maslahatBering(a) {
 // console.log("passed here 1");
 
 
-// call via asyn/await
-async function run() {
-    let javob = await maslahatBering(25);
-    console.log(javob);
-    javob = await maslahatBering(70);
-    console.log(javob);
-    javob = await maslahatBering(41);
-    console.log(javob);
-}
-run();
+// // call via asyn/await
+// async function run() {
+//     let javob = await maslahatBering(25);
+//     console.log(javob);
+//     javob = await maslahatBering(70);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
 
 
 
