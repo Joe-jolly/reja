5ff01753d0a9a56db2d15a29248d12f44c99bc13
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2024-11-24 17:57:40 +0900

    feat: CRUD operations

--- server.js
+++ server.js
@@ -1,49 +1,28 @@
-console.log('web serverni boshlash');
-const express = require("express");
-const res = require("express/lib/response");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data)
-    }
-});
+let db;
+const connectionString = 
+    "mongodb+srv://sardorbek_rakhmonov:sardorbek1210@cluster0.e8y9d.mongodb.net/Reja";
 
-// 1. Kirish codes
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+mongodb.connect(connectionString, {
+    userNewUrlParser: true, 
+    useUnifiedTopology: true,
+},
+(err, client) => {
+    if(err) console.log("ERROR on connection to MongoDB");
+        else {
+            console.log("MongoDB connection succeed");
+            module.exports = client;
 
-// 2: Session codes
-
-// 3. Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4. Routing codes
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({ test: "successful" });
-});
-
-app.get('/author', (req, res) => {
-        res.render("author", { user: user });
-    });
-
-app.get('/', function(req, res) {
-    res.render('reja');
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-    console.log(
-        `The server running successfully on PORT ${PORT}, http://localhost:${PORT}`
-    );
+            const app = require("./app");
+            const server = http.createServer(app);
+            let PORT = 3000;
+            server.listen(PORT, function () {
+                console.log(
+                    `The server running successfully on PORT ${PORT}, http://localhost:${PORT}`
+                );
+            });
+        }
 });
 
