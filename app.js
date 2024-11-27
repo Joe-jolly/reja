3fcd554f2da5c5d0dd953c7d7e9056c74f17e5c7
Joe-jolly <sardorbek.rakhmonov@gmail.com>
2024-11-28 02:55:59 +0900

    feat: FrontEnd JavaScriptini qurish

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const res = require("express/lib/response");
 const app = express();
 const fs = require("fs");
 
-let user;
+/* let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
     if(err) {
         console.log("ERROR:", err);
     } else {
         user = JSON.parse(data)
     }
-});
+});*/
 
 // MongoDB call
 const db = require("./server").db();
@@ -19,7 +19,7 @@ const db = require("./server").db();
 // 1. Kirish codes
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // 2: Session codes
 
@@ -32,13 +32,8 @@ app.post("/create-item", (req, res) => {
     console.log("user entered /create-item");
     console.log(req.body);
     const new_reja = req.body.reja;
-    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end('something went wrong');
-        } else {
-            res.end('successfully added');
-        }
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+       res.json(data.ops[0]);
     });
 });
 
